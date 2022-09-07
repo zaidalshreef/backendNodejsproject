762450123c540b74066e8dc8bc2b13e2f8051972
zaidalshreef <zaidalshreef@gmail.com>
2022-09-07 11:29:47 +0300

    finsh

--- controller/users.js
+++ controller/users.js
@@ -1,58 +1,53 @@
 const User = require("../model/user");
 
-
-
-module.exports.registerForm =  (req, res, next) => {
+module.exports.registerForm = (req, res, next) => {
   res.render("users/register");
-}
+};
 
-
-module.exports.createNewUser =  async (req, res, next) => {
-    try {
-      const { username, email, password,phoneNumber } = req.body;
-      const user = new User({
-        username,
-        email,
-        phoneNumber,
-      });
-      const registeredUser = await User.register(user, password);
-      req.login(registeredUser, (err) => {
-        if (err) return next(err);
-        req.flash("success", "You are now registered and logged in");
-        res.redirect("/campgrounds");
-      });
-    } catch (e) {
-      if (e.message.includes("phoneNumber")) {
+module.exports.createNewUser = async (req, res, next) => {
+  try {
+    const { username, email, password, phoneNumber } = req.body;
+    const user = new User({
+      username,
+      email,
+      phoneNumber,
+    });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      req.flash("success", "You are now registered and logged in");
+      res.redirect("/property");
+    });
+  } catch (e) {
+    if (e.message.includes("phoneNumber")) {
       req.flash("error", "PhoneNumber already in use");
-      }else if (e.message.includes("email")) {
-        req.flash("error", "Email already in use");      
-      } else {
+    } else if (e.message.includes("email")) {
+      req.flash("error", "Email already in use");
+    } else {
       req.flash("error", e.message);
-      }
-      return res.redirect("/register");
     }
+    return res.redirect("/register");
   }
+};
 
-  module.exports.loginForm =   (req, res, next) => {
+module.exports.loginForm = (req, res, next) => {
   res.render("users/login");
-}
+};
 
-module.exports.login =  (req, res, next) => {
-        const url = req.session.returnTo || "/property";
-        delete req.session.returnTo;
-        res.redirect(url);
-    }
+module.exports.login = (req, res, next) => {
+  const url = req.session.returnTo || "/property";
+  delete req.session.returnTo;
+  res.redirect(url);
+};
 
-
-module.exports.logout =  (req, res, next) => {
-  req.logout(function(err) {
-    if (err) { 
+module.exports.logout = (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
       return next(err);
-     }
     }
-    )
+  });
   req.flash("success", "You are logged out");
   const url = req.session.returnTo || "/property";
   delete req.session.returnTo;
   res.redirect(url);
-}
+};
